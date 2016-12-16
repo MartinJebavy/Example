1d59699f25f373204935dc61bf3ab5d23ad4167e
MartinJebavy <bc.martin.jebavy@gmail.com>
2016-12-16 11:45:03 +0100

    Add files via upload

---
+++ ASP. NET MVC/Controllers/LoginController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Realitni_Kancelar.DAL;
+using Realitni_Kancelar.Models;
+
+namespace Realitni_Kancelar.Controllers
+{
+    public class LoginController : Controller
+    {
+        private RealContext db = new RealContext();
+         
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login([Bind(Include = "ID,Nick,Password")]Person osoba)
+        {
+
+                var prihlasitOsobu = from a in db.Persons
+                                     where osoba.Nick == a.Nick
+                                     select a;
+                if (prihlasitOsobu.Count() != 0)
+                {
+                    if (osoba.Password == prihlasitOsobu.First().Password)
+                    {
+                    FormsAuthentication.SetAuthCookie(osoba.Nick, true);
+                    
+
+                   
+                    return RedirectToAction("Index", "Advertisement");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Nespravne heslo!");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Zadany nick neexistuje!");
+                }
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register([Bind(Include = "ID,FirstName,LastName,Contact,Nick,Password")]Person osoba)
+        {
+            if (ModelState.IsValid)
+            {
+                var duplicitniNick = from a in db.Persons
+                                     where osoba.Nick == a.Nick
+                                     select a;
+                if (duplicitniNick.Count() == 0)
+                {
+                    osoba.Role = TypeOfRole.User;
+                    db.Persons.Add(osoba);
+                    db.SaveChanges();
+                    Roles.AddUserToRole(osoba.Nick, "Admin");
+                    return RedirectToAction("Index","Person");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Nick je obsazeny, vyber jiny!");
+                }
+
+            }
+            return View();
+        }
+    }
+}
---
+++ ASP. NET MVC/DAL/RealInitializer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Realitni_Kancelar.Models;
+
+namespace Realitni_Kancelar.DAL
+{
+    public class RealInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<RealContext>
+    {
+        protected override void Seed(RealContext context)
+        {
+
+            var persons = new List<Person>
+            {
+new Person {FirstName = "Martin", LastName="Jebavý", Nick="Jeby", Password="12345",Role= TypeOfRole.Admin, Contact= "123 356 368" },
+new Person {FirstName = "Jiří", LastName="Škornok", Nick="Jirka", Password="1234", Role= TypeOfRole.User, Contact= "356 123 496" },
+new Person {FirstName = "Petr", LastName="Kopic", Nick="Petr", Password="123", Role= TypeOfRole.User, Contact= "695 365 146" }
+            };
+            persons.ForEach(s => context.Persons.Add(s));
+            context.SaveChanges();
+
+            var advertisements = new List<Advertisement>
+            {
+                new Advertisement {Title="Název1",TypeOfAd = TypeOfAdvertisement.Offer, VariantOfAd = VariantOfAdvertisement.Sale,
+                PropertyType = "Chalupa", DispositionOfProperty="3+1",PropertySize="Obytná plocha", ConditionOfProperty="Nový",
+                FinalPrice=1000000.50, PersonID=1},
+                new Advertisement {Title="Název2",TypeOfAd = TypeOfAdvertisement.Demand, VariantOfAd = VariantOfAdvertisement.Lease,
+                PropertyType = "Dům", DispositionOfProperty="5+2",PropertySize="Volná plocha", ConditionOfProperty="Rujna",
+                FinalPrice=2654333.50, PersonID=1},
+                new Advertisement {Title="Název3",TypeOfAd = TypeOfAdvertisement.Offer, VariantOfAd = VariantOfAdvertisement.Auction,
+                PropertyType = "Byt", DispositionOfProperty="1+KK",PropertySize="Užitková plocha", ConditionOfProperty="Po rekonstrukci",
+                FinalPrice=13365660.50, PersonID=2},
+                new Advertisement {Title="Název4",TypeOfAd = TypeOfAdvertisement.Demand, VariantOfAd = VariantOfAdvertisement.Lease,
+                PropertyType = "Zahrada", DispositionOfProperty="Neuvedeno",PropertySize="Obytná plocha", ConditionOfProperty="Rujna",
+                FinalPrice=50000, PersonID=3},
+                new Advertisement {Title="Název5",TypeOfAd = TypeOfAdvertisement.Demand, VariantOfAd = VariantOfAdvertisement.Sale,
+                PropertyType = "Byt", DispositionOfProperty="1+1",PropertySize="Zastavěná plocha", ConditionOfProperty="Nový",
+                FinalPrice=250000, PersonID=3}
+            };
+            advertisements.ForEach(s => context.Advertisements.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
